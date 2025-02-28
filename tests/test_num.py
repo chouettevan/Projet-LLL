3fc49a31b08976b76bdb381415246a2a89b20058
chouettevan <pybots743@gmail.com>
2025-02-27 19:38:34 -0500

    added failed test to test_num.py

--- tests/test_num.py
+++ tests/test_num.py
@@ -5,6 +5,7 @@ class Tests(unittest.TestCase):
     def test_pgcd(self):
         self.assertEqual(num.pgcd(60,40),20)
         self.assertEqual(num.pgcd(50,15),5)
+        self.assertEqual(num.pgcd(50,14),5)
 
 if __name__ == '__main__':
     unittest.main()
