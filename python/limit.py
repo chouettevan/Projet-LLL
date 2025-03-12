a9fe32e1a8dfd7e3db2bcc423295200ccb0f6720
chouettevan <pybots743@gmail.com>
2025-03-12 19:57:25 +0000

    modification du nom des fonctions et des bornes de la sommation

--- python/limit.py
+++ python/limit.py
@@ -1,15 +1,15 @@
 import math
-def sum_approx(func,n=1000):
+def summation(func,a,b):
     total = 0
-    for i in range(-n,n):
+    for i in range(a,b):
         total += func(i)
     return total
 
-def integral_approx(func,a,b,delta=0.001):
+def integral(func,a,b,delta=0.001):
     total = 0
     x = a
     while x < b:
-        total += func(x)
+        total += func(x)*delta
         a += delta
 
 def fourier_transform(func,delta=0.001,n=1000):
@@ -18,6 +18,6 @@ def fourier_transform(func,delta=0.001,n=1000):
             return func(x) * math.exp(-sqrt(-1)*2*math.pi*e*x)
         return t2
     def transform(freq):
-        return integral_approx(t1(freq),-n,n,delta)
+        return integral(t1(freq),-n,n,delta)
     return transform
 
