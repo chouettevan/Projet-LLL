9cce64ab4976630eb5c08185a22f2cc789820ea5
light <light@localhost.my.domain>
2025-03-19 13:53:26 -0400

    fix sur l'indentation

--- tests/test_limits.py
+++ tests/test_limits.py
@@ -5,18 +5,17 @@ import limit
 
 
 class Tests(unittest.TestCase):
-    def test_sommation(self):
-        identity = lambda x:x
-        self.assertEqual(limit.sum(identity,-100,100),0)
-        self.assertEqual(limit.sum(identity,0,100),5050)
-        self.assertEqual(limit.sum(abs,-100,100),5050*2)
-        self.assertEqual(limit.sum(abs,0,100),5050)
-    def test_integral(self):
-        self.assertGreater(limit.integral(math.sin,-1,1),-0.01) 
-        self.assertGreater(0.01,limit.integral(math.sin,-1,1)) 
+	def test_sommation(self):
+		identity = lambda x:x
+		self.assertEqual(limit.sum(identity,-100,100),0)
+		self.assertEqual(limit.sum(identity,0,100),5050)
+		self.assertEqual(limit.sum(abs,-100,100),5050*2)
+		self.assertEqual(limit.sum(abs,0,100),5050)
+	def test_integral(self):
+		self.assertGreater(limit.integral(math.sin,-1,1),-0.01) 
+		self.assertGreater(0.01,limit.integral(math.sin,-1,1)) 
 	def test_fourier_transform(self):
-		self.assertGreater(limit.fourier_transform(math.sin)(1),10**6)
-
+		self.assertGreater(limit.fourier_transform(math.sin)(1),1000)
 
 if __name__ == '__main__':
     unittest.main()
