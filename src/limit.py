46ab848c78e0da49a8615b15c028888fce7b84c5
light <light@localhost.my.domain>
2025-03-19 13:55:11 -0400

    fix: remplacer sqrt par math.sqrt

--- src/limit.py
+++ src/limit.py
@@ -16,7 +16,7 @@ def integral(func,a,b,delta=0.001):
 def fourier_transform(func,delta=0.001,n=1000):
     def t1(e):
         def t2(x):
-            return func(x) * math.exp(-sqrt(-1)*2*math.pi*e*x)
+            return func(x) * math.exp(-math.sqrt(-1)*2*math.pi*e*x)
         return t2
     def transform(freq):
         return integral(t1(freq),-n,n,delta)
