ea022a5987767c63d9897f1c1203b1e6a612ffab
chouettevan <pybots743@gmail.com>
2025-04-02 14:57:52 -0400

    fix: syntaxe de num.py

--- src/num.py
+++ src/num.py
@@ -1,17 +1,17 @@
 import math
 
 def totient(x):
-	start = 2
-	arr = []
-	tested = False
-	while start <= x && (tested || start*start < x):
-		if x % start == 0:
-			tested = True
-			arr.append(start)
-			while x % start == 0:
-				x = int(x/start)
+	debut = 2
+	tab = []
+	test = False
+	while debut <= x and (test or debut*debut < x):
+		if x % debut == 0:
+			test = True
+			tab.append(debut)
+			while x % debut == 0:
+				x = int(x/debut)
 	r = x
-	for i in arr:
+	for i in tab:
 		r *= (1-1/i)
 	return int(r)
 
