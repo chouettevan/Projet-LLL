7e2172b77be88ec5f311053dd41822b2656cd7a6
chouettevan <pybots743@gmail.com>
2025-04-02 13:41:36 -0400

    fix: test totient function instead of gcd

--- tests/test_num.py
+++ tests/test_num.py
@@ -3,8 +3,9 @@ from maravilla import num
 
 class Tests(unittest.TestCase):
     def test_pgcd(self):
-        self.assertEqual(num.pgcd(60,40),20)
-        self.assertEqual(num.pgcd(50,15),5)
+        self.assertEqual(num.totient(10),4)
+        self.assertEqual(num.totient(50),20)
+        self.assertEqual(num.totient(125),100)
 
 if __name__ == '__main__':
     unittest.main()
